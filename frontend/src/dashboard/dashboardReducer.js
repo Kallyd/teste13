883116fb4430241a6de8d820892bda5d7ef592ee
AngelinaPierre <gaptp95@gmail.com>
2023-05-09 18:57:16 -0300

    feat(actionCreator): connecting action creator to dashboard reducer

--- frontend/src/dashboard/dashboardReducer.js
+++ frontend/src/dashboard/dashboardReducer.js
@@ -8,5 +8,12 @@ const INITIAL_STATE = {
 }
 
 export default function (state = INITIAL_STATE, action) {
+    switch (action.type) {
+        case 'BILLING_SUMMARY_FETCHED':
+            return {
+                ...state,
+                summary: action.payload.data
+            }
+    }
     return state;
 }
