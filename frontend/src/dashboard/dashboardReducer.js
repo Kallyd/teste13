2dae279a1f5559d831ceb49617e2fd4f54ec300a
AngelinaPierre <gaptp95@gmail.com>
2023-05-09 18:51:37 -0300

    feat(redux): created reducer for dashboard with initial state

---
+++ frontend/src/dashboard/dashboardReducer.js
@@ -0,0 +1,12 @@
+// reducer do dashboard
+
+const INITIAL_STATE = {
+    summary: {
+        credit: 0,
+        debt: 0
+    }
+}
+
+export default function (state = INITIAL_STATE, action) {
+    return state;
+}
--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,13 +1,15 @@
 import { combineReducers } from 'redux';
+import dashboardReducer from '../dashboard/dashboardReducer';
 
 const rootReducer = combineReducers({
-    dashboard: () => ({
-        // reducer com estado fixo
-        summary: {
-            credit: 100,
-            debt: 50
-        }
-    })
+    // dashboard: () => ({
+    //     // reducer com estado fixo
+    //     summary: {
+    //         credit: 100,
+    //         debt: 50
+    //     }
+    // })
+    dashboard: dashboardReducer
 })
 
 export default rootReducer;
