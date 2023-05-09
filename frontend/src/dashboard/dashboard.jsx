4eb140129c22d48e887999efa86cc3b685e4d1be
AngelinaPierre <gaptp95@gmail.com>
2023-05-09 18:48:43 -0300

    feat(redux): integration of dashboard with redux static values

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,37 +1,49 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
 import ContentHeader from "../common/template/contentHeader";
 import Content from "../common/template/content";
 import ValueBox from "../common/widget/valueBox";
+import Row from "../common/layout/row";
 
-export default class Dashboard extends Component {
+class Dashboard extends Component {
 	render() {
+		const { credit, debt } = this.props.summary;
 		return (
 			<div>
 				<ContentHeader title="Dashboard" small="Version 1.0" />
 				<Content>
-					<ValueBox
-						cols="12 4"
-						color="green"
-						icon="bank"
-						value="R$ 10"
-						text="Total of Credit"
-					/>
-					<ValueBox
-						cols="12 4"
-						color="red"
-						icon="credit-card"
-						value="R$ 10"
-						text="Total of Debit"
-					/>
-					<ValueBox
-						cols="12 4"
-						color="blue"
-						icon="money"
-						value="R$ 0"
-						text="Consolidated Value"
-					/>
+					<Row>
+						<ValueBox
+							cols="12 4"
+							color="green"
+							icon="bank"
+							value={`${credit}`}
+							text="Total of Credit"
+						/>
+						<ValueBox
+							cols="12 4"
+							color="red"
+							icon="credit-card"
+							value={`${debt}`}
+							text="Total of Debt"
+						/>
+						<ValueBox
+							cols="12 4"
+							color="blue"
+							icon="money"
+							value={`${credit - debt}`}
+							text="Consolidated Value"
+						/>
+					</Row>
 				</Content>
 			</div>
 		);
 	}
 }
+
+const mapStateToProps = (state) => ({
+	summary: state.dashboard.summary,
+});
+
+export default connect(mapStateToProps)(Dashboard);
