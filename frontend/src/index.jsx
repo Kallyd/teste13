e754429d17d21825f4ce2d170ac6d0870e0cc57c
AngelinaPierre <gaptp95@gmail.com>
2023-05-10 09:53:29 -0300

    feat(devtools): added redux devTools for the application in index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -10,8 +10,11 @@ import promise from "redux-promise";
 import App from "./main/app";
 import reducers from "./main/reducers";
 
+const devtTools =
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
 // const store = createStore(reducers);
-const store = applyMiddleware(promise)(createStore)(reducers);
+const store = applyMiddleware(promise)(createStore)(reducers, devtTools);
 
 ReactDOM.render(
 	<Provider store={store}>
