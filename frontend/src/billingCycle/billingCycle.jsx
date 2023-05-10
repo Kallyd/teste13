5280cf8ed52b207b7bedac72fffb6c9250c5121b
AngelinaPierre <gaptp95@gmail.com>
2023-05-10 10:15:05 -0300

    feat(tabs): creating structural components for tabs

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -2,13 +2,21 @@ import React, { Component } from "react";
 
 import ContentHeader from "../common/template/contentHeader";
 import Content from "../common/template/content";
+import Tabs from "../common/tab/tabs";
+import TabsHeader from "../common/tab/tabsHeader";
+import TabsContent from "../common/tab/tabsContent";
 
 class BillingCycles extends Component {
 	render() {
 		return (
 			<div>
 				<ContentHeader title="Payments Cycles" small="Register" />
-				<Content>Payments Cycles</Content>
+				<Content>
+					<Tabs>
+						<TabsHeader></TabsHeader>
+						<TabsContent></TabsContent>
+					</Tabs>
+				</Content>
 			</div>
 		);
 	}
