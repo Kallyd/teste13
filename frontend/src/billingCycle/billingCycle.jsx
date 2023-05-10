ca073b4a1474820d52ed13d0d8d8320895cce111
AngelinaPierre <gaptp95@gmail.com>
2023-05-10 10:22:38 -0300

    feat(tabs): creating component tabHeader.jsx that will be used inside the component tabsHeader.jsx

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -5,6 +5,7 @@ import Content from "../common/template/content";
 import Tabs from "../common/tab/tabs";
 import TabsHeader from "../common/tab/tabsHeader";
 import TabsContent from "../common/tab/tabsContent";
+import TabHeader from "../common/tab/tabHeader";
 
 class BillingCycles extends Component {
 	render() {
@@ -13,7 +14,12 @@ class BillingCycles extends Component {
 				<ContentHeader title="Payments Cycles" small="Register" />
 				<Content>
 					<Tabs>
-						<TabsHeader></TabsHeader>
+						<TabsHeader>
+							<TabHeader label="List" icon="bars" target="tabList" />
+							<TabHeader label="Include" icon="plus" target="tabCreate" />
+							<TabHeader label="Edit" icon="pencil" target="tabUpdate" />
+							<TabHeader label="Exclude" icon="trash-o" target="tabDelete" />
+						</TabsHeader>
 						<TabsContent></TabsContent>
 					</Tabs>
 				</Content>
---
+++ frontend/src/common/tab/tabHeader.jsx
@@ -0,0 +1,18 @@
+// component that will be inside the tabsHeader
+
+import React, { Component } from "react";
+
+export default class TabHeader extends Component {
+	render() {
+		return (
+			<li>
+				<a
+					href="javascript:;"
+					data-toggle="tab"
+					data-target={this.props.target}>
+					<i className={`fa fa-${this.props.icon}`}></i> {this.props.label}
+				</a>
+			</li>
+		);
+	}
+}
