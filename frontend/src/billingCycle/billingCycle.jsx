d4281bb97a65dba3c5ef713c195208bc377fad64
AngelinaPierre <gaptp95@gmail.com>
2023-05-10 10:10:10 -0300

    fix(refactor): refactor of component BillingCycle.jsx to a class component

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,6 +1,16 @@
-import React from "react";
-export default (props) => (
-	<div>
-		<h1>Payment Cycle</h1>
-	</div>
-);
+import React, { Component } from "react";
+
+import ContentHeader from "../common/template/contentHeader";
+import Content from "../common/template/content";
+
+class BillingCycles extends Component {
+	render() {
+		return (
+			<div>
+				<ContentHeader title="Payments Cycles" small="Register" />
+				<Content>Payments Cycles</Content>
+			</div>
+		);
+	}
+}
+export default BillingCycles;
