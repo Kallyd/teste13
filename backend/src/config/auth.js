f830042e68108714bcf25fc1d9a696c84b1808f6
AngelinaPierre <gaptp95@gmail.com>
2023-05-12 13:17:48 -0300

    feat(mdd): created an middleware to authenticate the authTOken

---
+++ backend/src/config/auth.js
@@ -0,0 +1,29 @@
+// Middleware to filter to the authTOken that we created be the only one permited
+const jwt = require('jsonwebtoken');
+const env = require('../.env');
+
+module.exports = (req, res, next) => {
+    // CORS preflight request
+    if (req.method === 'OPTIONS') {
+        next();
+    } else {
+        // places where the token can be retrived
+        const token = req.body.token || req.query.token || req.headers['authorization'];
+        if (!token) {
+            return res.status(403).send({ errors: ['No token provided.'] });
+        }
+
+        jwt.verify(token, env.authSecret, function (err, decoded) {
+            if (err) {
+                return res.status(404).send({
+                    errors: ['Failed to authenticate token.']
+                })
+            } else {
+                // req.decoded = decoded;
+                next();
+            }
+        })
+
+    }
+
+}
