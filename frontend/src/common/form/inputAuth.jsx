163d9dcdfcc9324f9dd5637e3cc4492a86d1b983
AngelinaPierre <gaptp95@gmail.com>
2023-05-12 15:30:07 -0300

    feat(authInput): created component to hold the login input

---
+++ frontend/src/common/form/inputAuth.jsx
@@ -0,0 +1,19 @@
+// input based on adminLTE template
+import React from "react";
+import If from "../operator/if";
+
+export default (props) => (
+	<If test={!props.hide}>
+		<div className="form-group has-feedback">
+			<input
+				{...props.input}
+				type={props.type}
+				className="form-control"
+				placeholder={props.placeholder}
+				readOnly={props.readOnly}
+			/>
+			<span
+				className={`glyphicon glyphicon-${props.icon} form-control-feedback`}></span>
+		</div>
+	</If>
+);
